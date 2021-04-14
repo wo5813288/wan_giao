025af76eb542421b4014cfb2a74746a4e53a212f
wo5813288 <414051627@qq.com>
2021-04-14 18:51:19 +0800

    Main主题修改

--- lib/default/default.dart
+++ lib/default/default.dart
@@ -1,22 +1,26 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'file:///D:/Soft/AndroidWork/wan_android/lib/page/main/index_page.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   final routers = {
-    '/': (context) =>IndexPage(),
+    '/': (context) => IndexPage(),
   };
+
   @override
   Widget build(BuildContext context) {
-    SystemUiOverlayStyle _style = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemUiOverlayStyle _style =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(_style);
     return RefreshConfiguration(
-      hideFooterWhenNotFull:false,
-      child:  MaterialApp(
+      hideFooterWhenNotFull: false,
+      child: MaterialApp(
+        theme: ThemeData(
+            primaryColor: Colors.blue,
+            splashColor: Colors.white12,
+            highlightColor: Colors.blueAccent),
         localizationsDelegates: [
           // 这行是关键
           RefreshLocalizations.delegate,
@@ -26,12 +30,8 @@ class MyApp extends StatelessWidget{
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: routers,
-        supportedLocales: [
-          const Locale('zh')
-        ],
+        supportedLocales: [const Locale('zh')],
       ),
     );
-
   }
-
 }
